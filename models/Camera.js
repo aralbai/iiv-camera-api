4c96b0a409fee88be2ba597333b593aa4c903763
aralbai <xabibullayevmm@gmail.com>
2025-07-01 20:25:37 +0500

    changed

--- models/Camera.js
+++ models/Camera.js
@@ -1,24 +1,27 @@
 import mongoose from "mongoose";
 
-const cameraSchema = new mongoose.Schema({
-  cameraType: {
-    type: String,
-    required: true,
-    enum: ["ptz", "radar", "lis"],
-  },
-  address: {
-    type: String,
-  },
-  position: {
-    type: [Number],
-    required: true,
-    validate: {
-      validator: function (val) {
-        return val.length === 2;
+const cameraSchema = new mongoose.Schema(
+  {
+    cameraType: {
+      type: String,
+      required: true,
+      enum: ["ptz", "radar", "lis"],
+    },
+    address: {
+      type: String,
+    },
+    position: {
+      type: [Number],
+      required: true,
+      validate: {
+        validator: function (val) {
+          return val.length === 2;
+        },
+        message: "Position must be [longitude, latitude]",
       },
-      message: "Position must be [longitude, latitude]",
     },
   },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Camera", cameraSchema);
